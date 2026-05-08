bd90d063343d0ee6e1cda3afa3356287dbe832fd
SauloSSM <saulostuque3@gmail.com>
2026-05-07 23:24:16 -0300

    feat: specialize toString format for Expense and Revenue

--- src/Expense.java
+++ src/Expense.java
@@ -1,14 +1,15 @@
 public class Expense extends Transaction {
 
-    private String dueDate;
+    private final String dueDate;
 
-    public Expense(String category, String description, String date, double amount, String dueDate) {
-        super(category, description, date, amount);
+    public Expense(String description, String date, double amount, String dueDate) {
+        super("[DESPESA]", description, date, amount);
         this.dueDate = dueDate;
     }
 
     @Override
     public String toString() {
-        return "[Expense] " + super.toString() + " | Vence em: " + dueDate;
+        return String.format("%-10s %s Vencimento: %-12s", "[GASTO]",
+                super.toString(), dueDate);
     }
 }
--- src/Revenue.java
+++ src/Revenue.java
@@ -1,14 +1,15 @@
 public class Revenue extends Transaction {
 
-    private String source;
+    private final String source;
 
-    public Revenue(String category, String description, String date, double amount, String source) {
-        super(category, description, date, amount);
+    public Revenue(String description, String date, double amount, String source) {
+        super("[GANHO]", description, date, amount);
         this.source = source;
     }
 
     @Override
     public String toString() {
-        return "[Revenue] " + super.toString() + " | Recebe de: " + source;
+        return String.format("%-10s %s Recebimento: %-12s", "[RECEITA]",
+                super.toString(), source);
     }
 }
