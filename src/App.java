65ce5d445d4ca4fd54ac0a0f906bfbcdff74a196
SauloSSM <saulostuque3@gmail.com>
2026-05-07 23:26:20 -0300

    feat: add Scanner and prepare main for dynamic input

--- src/App.java
+++ src/App.java
@@ -4,14 +4,10 @@ public class App {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Transaction transaction = new Transaction("Cartão Bradesco/ Crédito", "Despesa",
-                "08/05/2026", 1000);
-        System.out.println(transaction);
-
-        Expense expense = new Expense("Cartão Polo", "Despesa", "06/05/2026", 545, "20/05/2026");
+        Expense expense = new Expense("Energia CPFL", "07/05/2026", 100, "20/05/2026");
         System.out.println(expense);
 
-        Revenue revenue = new Revenue("Cartão Caixa", "Receita", "08/05/2026", 1600, "Estágio X");
+        Revenue revenue = new Revenue("Salário", "08/05/2026", 1600, "Estágio X");
         System.out.println(revenue);
     }
 }
