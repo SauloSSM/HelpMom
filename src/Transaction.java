3662c750481a0023c12d352c037a34dcb4d5699c
SauloSSM <saulostuque3@gmail.com>
2026-05-07 23:20:57 -0300

    refactor: add string treatment for unexpected null output

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,32 +1,44 @@
 public class Transaction {
 
-    private String category;
-    private String description;
-    private String date;
-    private double amount;
+    private final String category;
+    private final String description;
+    private final String date;
+    private final double amount;
 
     public Transaction() {
-        this("Cartão","Despesa/Receita", "05/05/2026", 0);
+        this("Geral","Sem descrição", "00/00/0000", 0.0);
     }
 
     public Transaction(String category, String description, String date) {
-        this(category, description, date,0);
+        this(category, description, date,0.0);
     }
 
     public Transaction(String category, String description, String date, double amount) {
-        this.category = category;
-        this.description = description;
-        this.date = date;
+        this.category = (category == null || category.isBlank()) ? "Outros" : category;
+        this.description = (description == null || description.isBlank()) ? "Não Informado/a" : description;
+        this.date = (date == null || date.isBlank()) ? "Data Indefinida" : date;
         this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public double getAmount() {
+        return amount;
     }
 
     @Override
     public String toString() {
-        return "Transaction {" +
-                "category='" + category + '\'' +
-                ", description='" + description + '\'' +
-                ", date='" + date + '\'' +
-                ", amount=" + amount +
-                '}';
+        return String.format("| %-12s | %-20s | %-10s | R$ %10.2f |",
+                category, description, date, amount);
     }
 }
